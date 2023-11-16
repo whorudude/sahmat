50b29b12d99a381ae2c1466ab8d50752fc76e3a2
whorudude <arina.bendery@gmail.com>
2023-11-17 00:06:00 +0200

    basic positions

--- src/fe/App.js
+++ src/fe/App.js
@@ -1,4 +1,41 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const App = () => {
+  const [selectedCoordinate, setSelectedCoordinate] = useState('');
+  const [rooks, setRooks] = useState(Array(8).fill(null).map(() => Array(8).fill(false)));
+
+  //const handleAddRook = () => {
+   
+  //};
+
+  //const renderCells = () => {
+
+//};
+
+  //const renderRows = () => {
+  //};
+
+  return (
+    <div className="chessboard">
+      <div>
+        <label>
+          Coordonate:
+          <input
+            type="text"
+            value={selectedCoordinate}
+            onChange={(e) => setSelectedCoordinate(e.target.value.toUpperCase())}
+          />
+        </label>
+        <button onClick={handleAddRook}>Adaugă Turn</button>
+      </div>
+      <table>
+        <tbody>
+          {renderRows()}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
 export default App;
