90847beb066db633d63d51a0fa464c7b47985151
whorudude <arina.bendery@gmail.com>
2023-12-21 00:02:30 +0200

    platforma

--- src/fe/App.js
+++ src/fe/App.js
@@ -1,34 +1,75 @@
 import React, { useState } from 'react';
 import './App.css';
 
-  const App = () => {
+const App = () => {
+  const [selectedCoordinate, setSelectedCoordinate] = useState('');
+  const [rooks, setRooks] = useState(Array(8).fill(null).map(() => Array(8).fill(false)));
+
+  const isRookSafe = (row, col) => {
+    // Verificăm pe aceeași linie și coloană
+    for (let i = 0; i < 8; i++) {
+      if (rooks[row][i] || rooks[i][col]) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  /* const generateAllPossibleRooks = () => {
+    const generateRooksHelper = (row) => {
+      if (row === 8) {
+      
+            if (generateRooksHelper(row + 1)) {
+              setRooks(newRooks);
+              return true;
+            }
+            newRooks[row][col] = false; // Revertim modificările dacă nu am găsit o soluție
+          }
+        }
+      } else {
+        return generateRooksHelper(row + 1);
+      }
   
-    const handleClearRooks = () => {
-      setRooks(Array(8).fill(null).map(() => Array(8).fill(false)));
+      return false;
     };
-  
-    return (
-      <div className="chessboard">
-        <div>
-          <button onClick={handleClearRooks}>Șterge Toate</button>
-        </div>
-        <table>
-          <tbody>
-            {renderRows()}
-          </tbody>
-        </table>
-      </div>
-    );
+    generateRooksHelper(0);
+  }; */
+
   //const handleAddRook = () => {
    
-  //};
+  const handleClearRooks = () => {
+    setRooks(Array(8).fill(null).map(() => Array(8).fill(false)));
+  };
 
-  //const renderCells = () => {
-
-//};
-
-  //const renderRows = () => {
-  //};
+  const renderCells = () => {
+    const cells = [];
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        const cellClass = (i + j) % 2 === 0 ? 'white' : 'black';
+        const isRookPresent = rooks[i][j];
+  
+        cells.push(
+          <td key={j} className={`cell ${cellClass}`}>
+            {isRookPresent && <div className="rook">&#9814;</div>}
+          </td>
+        );
+      }
+    }
+    return cells;
+  };
+  
+  const renderRows = () => {
+    const rows = [];
+    for (let i = 0; i < 8; i++) {
+      rows.push(
+        <tr key={i}>
+          <td className="coordinates">{8 - i}</td>
+          {renderCells().slice(i * 8, (i + 1) * 8)}
+        </tr>
+      );
+    }
+    return rows;
+  };
 
   return (
     <div className="chessboard">
